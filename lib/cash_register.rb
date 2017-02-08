73c38ccd785460d4b37fb444fb3e91757be5e7f9
Your Name <you@example.com>
2017-02-08 08:34:28 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,28 @@
 
+class CashRegister
+  attr_accessor :total, :discount
+
+def intialize(discount=0)
+    @total=0
+    @discount=discount
+  end
+
+
+#def add_item(title,price,quantity)
+
+#  self.total+=price*quantity
+#end
+
+#def apply_discount
+#  "Your discount was applied,the total is $#{self.total}."
+#
+#def items
+#end
+
+#def void_last_transaction
+#  self.items-self.(last item we added)
+#end
+
+
+
+end
