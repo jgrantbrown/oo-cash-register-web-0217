276ea27d6297fcd1a41e64b2a9a92f4146b320de
Your Name <you@example.com>
2017-02-08 09:40:08 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,28 +1,35 @@
-
 class CashRegister
   attr_accessor :total, :discount
 
-def intialize(discount=0)
-    @total=0
-    @discount=discount
+
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items=[]
+    @price=[]
   end
 
+  def add_item(title, price, quantity = 1)
+      quantity.times { |item|  @items << title }
+      @total += price * quantity
+      @price<<price*quantity
+  end
 
-#def add_item(title,price,quantity)
+    def apply_discount
+      @total *= (100.0 - @discount)/100
+      if discount !=0
+        "After the discount, the total comes to $#{@total.to_i}."
+      else
+        "There is no discount to apply."
+      end
+    end
 
-#  self.total+=price*quantity
-#end
+    def items
+      @items
+    end
 
-#def apply_discount
-#  "Your discount was applied,the total is $#{self.total}."
-#
-#def items
-#end
-
-#def void_last_transaction
-#  self.items-self.(last item we added)
-#end
-
-
+    def void_last_transaction
+      @total-=@price[-1]
+    end
 
 end
